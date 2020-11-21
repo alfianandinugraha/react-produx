72269988470f58ff272a93f32e4218ce5ec2481a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 11:53:09 +0700

    add: create routing for every page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Route, Switch, HashRouter } from 'react-router-dom'
+import { AddProductPage } from './Pages/AddProductPage';
+import { HomePage } from './Pages/HomePage';
+import { ProductItemPage } from './Pages/ProductItemPage';
+import { ProductsPage } from './Pages/ProductsPage';
+import { UpdateProductPage } from './Pages/UpdateProductPage';
 
 function App() {
   return (
-    <div>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/add-product" exact>
+          <AddProductPage />
+        </Route>
+        <Route path="/products" exact>
+          <ProductsPage />
+        </Route>
+        <Route path="/products/:id" exact>
+          <ProductItemPage />
+        </Route>
+        <Route path="/products/:id/update" exact>
+          <UpdateProductPage />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
