4243f96679c7e37e140340cbae8a447e1b5c62d1
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 12:20:20 +0700

    add: adding link to add-product and products page

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { Button, makeStyles } from '@material-ui/core'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
-      marginRight: theme.spacing(2)
-    }
+      marginRight: theme.spacing(2),
+      textDecoration: 'none'
+    },
   }
 }))
 
@@ -16,8 +18,12 @@ export const HomePage = () => {
     <div>
       <h1>Dashboard Produx</h1>
       <div className={classes.root}>
-        <Button variant="contained" color="primary">Add Product</Button>
-        <Button variant="contained" color="primary">List Product</Button>
+        <Link to="/add-product">
+          <Button variant="contained" color="primary">Add Product</Button>
+        </Link>
+        <Link to="/products">
+          <Button variant="contained" color="primary">List Product</Button>
+        </Link>
       </div>
     </div>
   )
