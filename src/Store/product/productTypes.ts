8a2d12af878e19dbe36f9d78e9578467aeee7546
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 12:52:22 +0700

    add: create type and interface for Product store

--- src/Store/product/productTypes.ts
+++ src/Store/product/productTypes.ts
@@ -1,3 +1,18 @@
+export type ProductFetchTypes =
+  "FETCH_PRODUCT_REQUEST" |
+  "FETCH_PRODUCT_SUCCESS" |
+  "FETCH_PRODUCT_FAILURE"
+
+export interface ProductStore {
+  data?: Product[],
+  isLoading?: boolean,
+  error?: string
+}
+
+export interface ProductFetchAction extends ProductStore{
+  type: ProductFetchTypes
+}
+
 export interface Product {
   id: number,
   name: string,
@@ -6,3 +21,7 @@ export interface Product {
   createdAt: number,
   updatedAt: number
 }
+
+export const FETCH_PRODUCT_REQUEST: ProductFetchTypes = "FETCH_PRODUCT_REQUEST"
+export const FETCH_PRODUCT_SUCCESS: ProductFetchTypes = "FETCH_PRODUCT_SUCCESS"
+export const FETCH_PRODUCT_FAILURE: ProductFetchTypes = "FETCH_PRODUCT_FAILURE"
