75ce21223016fbea5195d5763c8c33a18bcf18db
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 11:55:44 +0700

    add: create root index for pages component

---
+++ src/Pages/index.ts
@@ -0,0 +1,13 @@
+import { HomePage } from './HomePage'
+import { AddProductPage } from './AddProductPage'
+import { UpdateProductPage } from './UpdateProductPage'
+import { ProductItemPage } from './ProductItemPage'
+import { ProductsPage } from './ProductsPage'
+
+export {
+  HomePage,
+  AddProductPage,
+  UpdateProductPage,
+  ProductItemPage,
+  ProductsPage
+}
