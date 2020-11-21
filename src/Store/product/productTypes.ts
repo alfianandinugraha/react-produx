65e78440f90e6bcc5211f2be898be86e4e53d4e5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 22:55:24 +0700

    add: adding type for send proruct

--- src/Store/product/productTypes.ts
+++ src/Store/product/productTypes.ts
@@ -3,6 +3,11 @@ export type ProductFetchTypes =
   "FETCH_PRODUCT_SUCCESS" |
   "FETCH_PRODUCT_FAILURE"
 
+export type SendProductTypes =
+  "SEND_PRODUCT_REQUEST" |
+  "SEND_PRODUCT_SUCCESS" |
+  "SEND_PRODUCT_FAILURE"
+
 export interface ProductStore {
   data?: Product[],
   isLoading?: boolean,
@@ -26,7 +31,13 @@ export const FETCH_PRODUCT_REQUEST: ProductFetchTypes = "FETCH_PRODUCT_REQUEST"
 export const FETCH_PRODUCT_SUCCESS: ProductFetchTypes = "FETCH_PRODUCT_SUCCESS"
 export const FETCH_PRODUCT_FAILURE: ProductFetchTypes = "FETCH_PRODUCT_FAILURE"
 
-export interface ProductAction extends ProductFetchAction { }
+export const SEND_PRODUCT_REQUEST: SendProductTypes = "SEND_PRODUCT_REQUEST"
+export const SEND_PRODUCT_SUCCESS: SendProductTypes = "SEND_PRODUCT_SUCCESS"
+export const SEND_PRODUCT_FAILURE: SendProductTypes = "SEND_PRODUCT_FAILURE"
+
+export interface ProductAction extends ProductStore { 
+  type: ProductFetchAction | SendProductTypes
+}
 
 export interface ProductProps {
   product: ProductStore,
