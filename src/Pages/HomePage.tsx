981a6ee1407d4554186b756f6c85806cf9b7df37
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 12:10:54 +0700

    add: create design for homepage

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,9 +1,24 @@
+import { Button, makeStyles } from '@material-ui/core'
 import React from 'react'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginRight: theme.spacing(2)
+    }
+  }
+}))
+
 export const HomePage = () => {
+  const classes = useStyles()
+
   return (
     <div>
-      <h1>Hello this is HomePage</h1>
+      <h1>Dashboard Produx</h1>
+      <div className={classes.root}>
+        <Button variant="contained" color="primary">Add Product</Button>
+        <Button variant="contained" color="primary">List Product</Button>
+      </div>
     </div>
   )
 }
