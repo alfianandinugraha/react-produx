504a499764c8cfad8a5bb06b4df7d4da4e7f9f1d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 15:07:59 +0700

    add: make conditional when product is not found

--- src/Pages/ProductsPage.tsx
+++ src/Pages/ProductsPage.tsx
@@ -1,20 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { connect } from 'react-redux'
 import React, { useEffect } from 'react'
 import { ProductProps, ProductStore } from '../Store/product/productTypes'
 import { fetchProductRequest } from '../Store/product/productActions.fetch'
+import { ProductsTable } from '../Components/ProductsTable'
 
 const ProductsPage = (props: ProductProps) => {
   useEffect(() => {
-    props.fetchProductRequest && props.fetchProductRequest()
+    props.fetchProductRequest()
   }, [])
 
-  useEffect(() => {
-    console.log(props.product)
-  }, [props.product])
+  const ToggleComponentProduct = () => {
+    if (props.product.error) return (<p>Error : {props.product.error}</p>)
+
+    return props.product.data !== undefined && props.product.data.length !== 0 ?
+      (<ProductsTable products={props.product.data} />) :
+      (<p>Product not Found</p>)
+  }
 
   return (
     <div>
       <h1>Products</h1>
+      <div>
+        <ToggleComponentProduct />
+      </div>
     </div>
   )
 }
