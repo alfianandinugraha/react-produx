e1d806253c97fd24f6d01540eaa5ca19cd8f81aa
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-21 17:29:09 +0700

    add: create receivePayload to receive payload from ProductForm

--- src/Pages/AddProductPage.tsx
+++ src/Pages/AddProductPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { ProductForm } from '../Components/ProductForm'
+import { Product } from '../Store/product/productTypes'
 
 export const AddProductPage = () => {
+  const receivePayload = (payload: Product) => {
+    console.log(payload)
+  }
+
   return (
     <div>
       <h1>Add Product</h1>
-      <ProductForm />
+      <ProductForm payloadHandler={receivePayload}/>
     </div>
   )
 }
