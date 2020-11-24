48ad6c1adb71db2eb4871781cae7835deb5493b4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-24 15:35:38 +0700

    add: make saga and request to update product

--- src/Store/product/productActions.update.ts
+++ src/Store/product/productActions.update.ts
@@ -1,23 +1,42 @@
-import { DELETE_PRODUCT_FAILURE, DELETE_PRODUCT_REQUEST, DELETE_PRODUCT_SUCCESS, Product, ProductAction } from './productTypes';
+import Axios from 'axios';
+import { put, takeEvery } from 'redux-saga/effects';
+import {  Product, ProductAction, UPDATE_PRODUCT_FAILURE, UPDATE_PRODUCT_REQUEST, UPDATE_PRODUCT_SUCCESS } from './productTypes';
 
-export function deleteProductRequest(product: Product): ProductAction {
+export function updateProductRequest(product: Product): ProductAction {
   return {
-    type: DELETE_PRODUCT_REQUEST,
+    type: UPDATE_PRODUCT_REQUEST,
     payload: [product]
   }
 }
 
-export function deleteProductSuccess(product: Product): ProductAction {
+export function updateProductSuccess(product: Product): ProductAction {
   return {
-    type: DELETE_PRODUCT_SUCCESS,
+    type: UPDATE_PRODUCT_SUCCESS,
     payload: [product]
   }
 }
 
-export function deleteProductFailure(error: string): ProductAction {
+export function updateProductFailure(error: string): ProductAction {
   return {
-    type: DELETE_PRODUCT_FAILURE,
+    type: UPDATE_PRODUCT_FAILURE,
     payload: [],
     message: error
   }
+}
+
+function* updateProduct(action: ProductAction) {
+  const { payload } = action
+  const [product] = payload
+  const {id} = product
+
+  try {
+    yield Axios.put(`http://localhost:5500/products/${id}`, product)
+    yield put(updateProductSuccess(product))
+  } catch (e) {
+    yield put(updateProductFailure(e.message))
+  }
+}
+
+export function* watchUpdateProduct() {
+  yield takeEvery(UPDATE_PRODUCT_REQUEST, updateProduct)
 }
--- src/Store/product/productSagas.ts
+++ src/Store/product/productSagas.ts
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects'
 import { watchDeleteProduct } from './productActions.delete'
 import { watchFetchProduct } from './productActions.fetch'
 import { watchSendProduct } from './productActions.send'
+import { watchUpdateProduct } from './productActions.update'
 
 export function* productRootSaga() {
-  yield all([watchFetchProduct(), watchSendProduct(), watchDeleteProduct()])
+  yield all([watchFetchProduct(), watchSendProduct(), watchDeleteProduct(), watchUpdateProduct()])
 }
