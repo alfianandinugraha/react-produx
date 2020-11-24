654b995dd895bde4086dbbf888bc279dbd76e0b8
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-24 13:36:50 +0700

    add: create types for delete product action

--- src/Store/product/productTypes.ts
+++ src/Store/product/productTypes.ts
@@ -8,6 +8,11 @@ export type SendProductTypes =
   "SEND_PRODUCT_SUCCESS" |
   "SEND_PRODUCT_FAILURE"
 
+export type DeleteProductTypes =
+  "DELETE_PRODUCT_REQUEST" |
+  "DELETE_PRODUCT_SUCCESS" |
+  "DELETE_PRODUCT_FAILURE"
+
 export interface ProductStore {
   data?: Product[],
   isLoading?: boolean,
@@ -31,10 +36,18 @@ export const SEND_PRODUCT_REQUEST: SendProductTypes = "SEND_PRODUCT_REQUEST"
 export const SEND_PRODUCT_SUCCESS: SendProductTypes = "SEND_PRODUCT_SUCCESS"
 export const SEND_PRODUCT_FAILURE: SendProductTypes = "SEND_PRODUCT_FAILURE"
 
+export const DELETE_PRODUCT_REQUEST: DeleteProductTypes = "DELETE_PRODUCT_REQUEST"
+export const DELETE_PRODUCT_SUCCESS: DeleteProductTypes = "DELETE_PRODUCT_SUCCESS"
+export const DELETE_PRODUCT_FAILURE: DeleteProductTypes = "DELETE_PRODUCT_FAILURE"
+
 export interface ProductAction { 
-  type: FetchProductTypes | SendProductTypes,
+  type: FetchProductTypes | SendProductTypes | DeleteProductTypes,
   payload: Product[],
   message?: string
+}
+
+export interface DeleteProductProps {
+  deleteProductRequest: (product: Product) => void
 }
 
 export interface SendProductProps {
