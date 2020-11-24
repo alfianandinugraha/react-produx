ac3cc5ece99e185d0a84f346af25241223f7e48d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-24 14:18:13 +0700

    add: make fetch product based on param

--- src/Pages/UpdateProductPage.tsx
+++ src/Pages/UpdateProductPage.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Product } from '../Store/product/productTypes'
 
 export const UpdateProductPage = () => {
+  const params = useParams<{ id: string }>()
+  const id = +params.id
+
+  const [product, setProduct] = useState<Product>()
+
+  useEffect(() => {
+    Axios.get(`http://localhost:5500/products/${id}`)
+      .then(({ data }) => setProduct(data))
+      .catch(err => console.log({ err }))
+  }, [])
+
+  useEffect(() => {
+    console.log({product})
+  }, [product])
+
   return (
     <div>
-      <h1>Hello this is UpdateProductPage</h1>
+      <h1>Update Product #{id}</h1>
     </div>
   )
 }
