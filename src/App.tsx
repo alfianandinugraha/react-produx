75e225405100ffca4bfbf2efb2bb8fc0abd8e3b9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-24 21:11:07 +0700

    add: make NavigationTop as global component for every page

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,21 @@ function App() {
   return (
     <Container maxWidth="md">
       <HashRouter>
+        <NavigationTop />
         <Switch>
           <Route path="/" exact>
             <HomePage />
           </Route>
           <Route path="/add-product" exact>
-            <NavigationTop />
             <AddProductPage />
           </Route>
           <Route path="/products" exact>
-            <NavigationTop />
             <ProductsPage />
           </Route>
           <Route path="/products/:id" exact>
-            <NavigationTop />
             <ProductItemPage />
           </Route>
           <Route path="/products/:id/update" exact>
-            <NavigationTop />
             <UpdateProductPage />
           </Route>
         </Switch>
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,30 +1,9 @@
-import { Button, makeStyles } from '@material-ui/core'
 import React from 'react'
-import { Link } from 'react-router-dom'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      marginRight: theme.spacing(2),
-      textDecoration: 'none'
-    },
-  }
-}))
 
 export const HomePage = () => {
-  const classes = useStyles()
-
   return (
     <div>
       <h1>Dashboard Produx</h1>
-      <div className={classes.root}>
-        <Link to="/add-product">
-          <Button variant="contained" color="primary">Add Product</Button>
-        </Link>
-        <Link to="/products">
-          <Button variant="contained" color="primary">List Product</Button>
-        </Link>
-      </div>
     </div>
   )
 }
